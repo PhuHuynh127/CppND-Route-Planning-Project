360e8fe142370d1b37ce096c68b4f65dd517fa23
Phu Huynh Van <phuynhvan54@gmail.com>
2025-02-28 16:54:27 +0700

    [cpp] project Route Planning

---
+++ src/route_planner.cpp
@@ -0,0 +1,68 @@
+#include "route_planner.h"
+#include <algorithm>
+
+RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
+    // Convert inputs to percentage:
+    start_x *= 0.01;
+    start_y *= 0.01;
+    end_x *= 0.01;
+    end_y *= 0.01;
+    start_node = &m_Model.FindClosestNode(start_x, start_y);
+    end_node = &m_Model.FindClosestNode(end_x, end_y);
+
+}
+
+float RoutePlanner::CalculateHValue(RouteModel::Node const *node) { 
+  return node->distance(*end_node);
+}
+
+void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
+    current_node->FindNeighbors();
+    for(RouteModel::Node* node : current_node->neighbors)
+    {
+        node->parent = current_node;
+        node->g_value = current_node->g_value + current_node->distance(*node);
+        node->h_value = CalculateHValue(node);
+        open_list.push_back(node);
+        node->visited = true;
+    }
+}
+
+RouteModel::Node *RoutePlanner::NextNode() {
+    std::sort(open_list.begin(), open_list.end(), [](const auto &a, const auto &b){
+    return a->h_value + a->g_value < b->h_value + b->g_value;});
+    RouteModel::Node* Lowest_Sum = open_list.front();
+    open_list.erase(open_list.begin());
+    return Lowest_Sum;
+}
+
+std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
+  distance = 0.0f;
+  std::vector<RouteModel::Node> path_found;
+  do
+  {
+    path_found.insert(path_found.begin(), *current_node);
+    if(current_node->parent == nullptr)
+    {
+      break;
+    }
+    distance += current_node->distance(*(current_node->parent));
+    current_node = current_node->parent;
+  }while(current_node);
+  distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
+  return path_found;
+}
+
+void RoutePlanner::AStarSearch() {
+    RouteModel::Node *current_node = start_node;
+
+    // TODO: Implement your solution here.
+    current_node->visited = true;
+    open_list.push_back(current_node);
+    while(!open_list.empty() && current_node != end_node)
+    {
+        current_node = NextNode();
+        AddNeighbors(current_node); 
+    }
+    m_Model.path = ConstructFinalPath(current_node);
+}
